e613f4835a0388cb976491c5d07990bc5c3ea689
Burati03 <gabrielhenrique@colegiopaulodetarso.com.br>
2025-11-05 16:07:12 +0000

    flask

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
 @app.route('/')
 def index():
     return("hello")
+
+@app.route('/info')
+def info():
+    return("Página info")
+
+@app.get("/info") 
+
+def info(): 
+
+   modulo = "Flask" 
+
+   aula = 1 
+
+   return f"<h1>Módulo: {modulo} — Aula {aula}<h1>"
+
+@app.get("/bemvindo/<usuario>") 
+
+def bemvindo(usuario):
+
+   return f"<h1>Bem-vindo, {usuario.capitalize()}!<h1>"
+
+from flask import Flask, redirect
+
+@app.get("/home") 
+
+def home(): 
+
+  return redirect("/")
